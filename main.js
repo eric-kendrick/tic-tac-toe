e80a27179c8321cb9d021439d07401b4f6dd8950
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-09-21 10:54:13 -0500

    Add function to check for winning board state

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //-------------- Query Selectors ------------//
-var boxes = document.querySelector('section > div');
-var playerOneView = document.querySelector('.player-one-header');
-var playerTwoView = document.querySelector('.player-two-header');
+var boxes = document.getElementsByClassName('box');
+var playerOneDisplay = document.querySelector('.player-one-header');
+var playerTwoDisplay = document.querySelector('.player-two-header');
 
 
 //------------- Event Listeners ------------//
@@ -9,28 +9,46 @@ var playerTwoView = document.querySelector('.player-two-header');
 
 
 //------------- Variables -------------------//
-var clickCounter = 0;
+var clickCount = 0;
+var players = ['X', 'O'];
+var currentPlayer = players[0];
 var winArray = [
-  [boxes[0], boxes[1], boxes[2]],
-  [boxes[3], boxes[4], boxes[5]],
-  [boxes[6], boxes[7], boxes[8]],
-  [boxes[0], boxes[3], boxes[6]],
-  [boxes[1], boxes[4], boxes[7]],
-  [boxes[2], boxes[5], boxes[8]],
-  [boxes[0], boxes[4], boxes[8]],
-  [boxes[6], boxes[4], boxes[2]]
+  [0, 1, 2], [3, 4, 5],
+  [6, 7, 8], [0, 3, 6],
+  [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
 ]
-
+  
 //------------- Data Functions --------------//
 
 //Create a function that creates a player object. Properties include id, player token and number of wins
 
-function createPlayer(id, token, wins) {
+function createPlayer(id, token) {
   return {
     id: id,
     token: token,
-    wins: wins
+    wins: 0
   }
 };
+
+// Build function to add player click on a board box. 
+// Add click classlist
+// Increment the click counter
+function addClick(e) {
+    clickCount++;
+    e.target.classList.add('clicked');
+}
+
+function checkWinArray(player) {
+  for (let i = 0; i < winArray.length; i++) {
+    var [one, two, three] = winArray[i];
+    if (boxes[one].innerHTML === player.token && boxes[two].innerHTML === player.token && 
+      boxes[three] === player.token) {
+        return true;
+      }
+  }
+  return false;
+}
+
 
 //-------------- DOM Functions ---------------//
