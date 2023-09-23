168102d97775272ebbdbfbe4369ed476bc2c7e5c
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-09-23 11:12:18 -0500

    Add game reset function and refactor checkForWin

--- main.js
+++ main.js
@@ -5,20 +5,13 @@ var playerTwoDisplay = document.querySelector('.player-two-header');
 
 
 //------------- Event Listeners ------------//
-
+document.addEventListener("DOMContentLoaded", checkForWin);
 
 
 //------------- Variables -------------------//
 var clickCount = 0;
-var players = ['X', 'O'];
-var currentPlayer = players[0];
-var winArray = [
-  [0, 1, 2], [3, 4, 5],
-  [6, 7, 8], [0, 3, 6],
-  [1, 4, 7], [2, 5, 8],
-  [0, 4, 8], [2, 4, 6]
-]
-  
+var playerOneTurn = true;
+
 //------------- Data Functions --------------//
 
 //Create a function that creates a player object. Properties include id, player token and number of wins
@@ -31,17 +24,24 @@ function createPlayer(id, token) {
   }
 };
 
-// Build function to add player click on a board box. 
-// Add click classlist
-// Increment the click counter
-function addClick(e) {
-    clickCount++;
-    e.target.classList.add('clicked');
-}
-
-function checkWinArray(player) {
-  for (let i = 0; i < winArray.length; i++) {
+function checkForWin(player) {
+  var winArray = [
+    [0, 1, 2], [3, 4, 5],
+    [6, 7, 8], [0, 3, 6],
+    [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
+  for (var i = 0; i < winArray.length; i++) {
+  
     var [one, two, three] = winArray[i];
+    console.log('//-----Boxes[1]-------//');
+    console.log(boxes[one]);
+    console.log('//-----Boxes[2]-------//');
+    console.log(boxes[two]);
+    console.log('//-----Boxes[3]-------//');
+    console.log(boxes[three]);
+
+
     if (boxes[one].innerHTML === player.token && boxes[two].innerHTML === player.token && 
       boxes[three] === player.token) {
         return true;
@@ -49,6 +49,19 @@ function checkWinArray(player) {
   }
   return false;
 }
+// // Reset function that brings the game back to its original state by clearing out boxes and returns player arr to first
+function resetGame() {
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML === '';
+    clickCount = 0;
+    currentPlayer = players[0];
+  }
+};
+
+function addClick() {
+  clickCount++;
+  e.target.classList.add('clicked');
+};
 
 
 //-------------- DOM Functions ---------------//
