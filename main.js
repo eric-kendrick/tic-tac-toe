433f59597993da94e10e681964ac401a458d10ce
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-09-24 11:54:16 -0500

    Add functions for box click, winner message, player turn change, and add functionality to checkForWin

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 //-------------- Query Selectors ------------//
-var boxes = document.getElementsByClassName('box');
+var boxes = document.querySelectorAll('section > div'), clickCount = 0;
 var playerOneDisplay = document.querySelector('.player-one-header');
 var playerTwoDisplay = document.querySelector('.player-two-header');
+var messageDisplay = document.querySelector('.board-message');
+var resetBtn = document.querySelector('.reset-btn');
+
+//------------- Variables -------------------//
+var players = [];
+var currentPlayer;
 
 
 //------------- Event Listeners ------------//
-document.addEventListener("DOMContentLoaded", checkForWin);
+document.addEventListener("DOMContentLoaded", addPlayers);
+boxes.forEach(function (box) {
+  box.addEventListener('click', boxClicked);
+});
+// resetBtn.addEventListener('click', resetGame)// <-----For testing reset functionality;
 
-
-//------------- Variables -------------------//
-var clickCount = 0;
-var playerOneTurn = true;
 
 //------------- Data Functions --------------//
-
-//Create a function that creates a player object. Properties include id, player token and number of wins
 
 function createPlayer(id, token) {
   return {
@@ -24,44 +28,70 @@ function createPlayer(id, token) {
   }
 };
 
+function addPlayers() {
+  var player1 = createPlayer(1, "X");
+  var player2 = createPlayer(2, "O");
+  players.push(player1, player2);
+  currentPlayer = player1;
+};
+
+function changePlayer() {
+  currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+  messageDisplay.innerText = `${currentPlayer.token} turn`;
+};
+
 function checkForWin(player) {
-  var winArray = [
+  var winArrays = [
     [0, 1, 2], [3, 4, 5],
     [6, 7, 8], [0, 3, 6],
     [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
   ]
-  for (var i = 0; i < winArray.length; i++) {
-  
-    var [one, two, three] = winArray[i];
-    console.log('//-----Boxes[1]-------//');
-    console.log(boxes[one]);
-    console.log('//-----Boxes[2]-------//');
-    console.log(boxes[two]);
-    console.log('//-----Boxes[3]-------//');
-    console.log(boxes[three]);
+  for (let i = 0; i < winArrays.length; i++) {
+    var winCombos = winArrays[i];
+    var one = boxes[winCombos[0]];
+    var two = boxes[winCombos[1]];
+    var three = boxes[winCombos[2]];
 
-
-    if (boxes[one].innerHTML === player.token && boxes[two].innerHTML === player.token && 
-      boxes[three] === player.token) {
-        return true;
+    if (one.innerHTML === player.token && two.innerHTML === player.token &&
+      three.innerHTML === player.token) {
+        winnerMessage(player);
+        setTimeout(function() {
+          resetGame();
+        }, 2000);
       }
-  }
-  return false;
-}
-// // Reset function that brings the game back to its original state by clearing out boxes and returns player arr to first
-function resetGame() {
-  for (var i = 0; i < boxes.length; i++) {
-    boxes[i].innerHTML === '';
-    clickCount = 0;
-    currentPlayer = players[0];
   }
 };
 
-function addClick() {
+function resetGame() {
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].innerText = '';
+    boxes[i].classList.remove('clicked');
+    changePlayer();
+  }
+};
+
+function addClick(e) {
   clickCount++;
   e.target.classList.add('clicked');
 };
 
-
 //-------------- DOM Functions ---------------//
+
+function boxClicked(e) {
+  if (clickCount >= 9) {
+    resetGame();
+  } else {
+    if (e.target.classList.contains('clicked') === false) {
+      addClick(e);
+      e.target.innerHTML = currentPlayer.token;
+    }
+    checkForWin(currentPlayer);
+    changePlayer();
+  }
+}
+
+function winnerMessage(player) {
+  var winMessage = `Congrats! ${player.token} wins!`;
+  messageDisplay.innerText = winMessage;
+}
