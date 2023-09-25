0c7a7139131a9bfe1b2552860dee1c5865063849
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-09-25 13:24:33 -0500

    Separated game logic and DOM JS into two files

--- main.js
+++ main.js
@@ -1,39 +1,26 @@
-//-------------- Query Selectors ------------//
-var boxes = document.querySelectorAll('section > div');
-var messageDisplay = document.querySelector('.board-message');
-var resetBtn = document.querySelector('.reset-btn');
 
 //------------- Variables -------------------//
-
-var players = [];
-var startingPlayer;
-var currentPlayer;
 var clickCount = 0;
+var players = [];
+var currentPlayer;
+var startingPlayer;
 
-
-//------------- Event Listeners ------------//
-
-document.addEventListener("DOMContentLoaded", addPlayers);
-boxes.forEach(function (box) {
-  box.addEventListener('click', boxClicked);
-});
-
-
-//------------- Data Functions --------------//
-
+//------------- Functions --------------//
 function createPlayer(id, token) {
   return {
     id: id,
     token: token,
     wins: 0
-  };
+  }
 };
 
 function addPlayers() {
-  var player1 = createPlayer(1, "X");
-  var player2 = createPlayer(2, "O");
+  var player1 = createPlayer(1, 'X');
+  var player2 = createPlayer(2, 'O');
+
   players.push(player1, player2);
   playersView(players);
+
   currentPlayer = player1;
   currentPlayerMessage();
 };
@@ -49,12 +36,13 @@ function checkForWin(player) {
   for (let i = 0; i < winArrays.length; i++) {
     var winCombos = winArrays[i];
     var[one, two, three] = [boxes[winCombos[0]], boxes[winCombos[1]], boxes[winCombos[2]]];
+
     if (one.innerHTML === player.token && two.innerHTML === player.token &&
       three.innerHTML === player.token) {
         increaseWins(player);
         winnerMessage(player);
         resetGame();
-      } else if (clickCount === 9 && messageDisplay.innerText.includes('wins') === false) {
+      } else if (clickCount === 9 && !messageDisplay.innerText.includes('wins')) {
         drawMessage();
       }
   }
@@ -82,8 +70,6 @@ function increaseWins(player) {
   player.wins++;
 };
 
-//-------------- DOM Functions ---------------//
-
 function boxClicked(e) {
   if (e.target.classList.contains('clicked') === false) {
     addClick(e);
@@ -98,37 +84,4 @@ function boxClicked(e) {
   }
 };
 
-function changePlayer() {
-  currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
-  messageDisplay.innerText = `${currentPlayer.token} turn`;
-};
 
-function newGamePlayerChange() {
-  startingPlayer = startingPlayer === players[0] ? players[1] : players[0];
-}
-
-function winnerMessage(player) {
-  messageDisplay.innerHTML = `Congrats! ${player.token} wins!`;
-}
-
-function drawMessage() {
-  messageDisplay.innerHTML = `It's a draw!`;
-}
-
-function currentPlayerMessage() {
-  messageDisplay.innerHTML = `${currentPlayer.token} turn`;
-}
-
-function playersView(players) {
-  var playerOneDisplay = document.querySelector('.player-one-header');
-  playerOneDisplay.innerHTML = 
-  `<p class='player-token'>${players[0].token}</p>
-   <p class='player-win-view'>${players[0].wins} wins</p>
-  `;
-
-  var playerTwoDisplay = document.querySelector('.player-two-header');
-  playerTwoDisplay.innerHTML = 
-  `<p class='player-token'>${players[1].token}</p>
-   <p class='player-win-view'>${players[1].wins} wins</p>
-  `;
-};
